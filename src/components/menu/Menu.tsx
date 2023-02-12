3d8594948f2cf822c6fee31447e5552c6f01a563
Tyrtychko Roman <tyrtychko.r404man@gmail.com>
2023-02-12 12:19:00 +0600

    Menu desktop

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -2,15 +2,15 @@ import React, { useState } from "react";
 import "./menu.scss";
 
 function Menu() {
-  const [isOpen, setIsOpen] = useState(true);
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <div
       onMouseEnter={() => setIsOpen(true)}
       onMouseLeave={() => setIsOpen(false)}
-      className="menu"
+      className={isOpen ? "menu isOpen" : "menu"}
     >
-      <nav className={isOpen ? "isOpen" : ""}>
-        <a href="" className="nav-link">
+      <nav>
+        <a href="" className="nav-link active">
           <div className="link-icon">
             <svg
               width="32"
@@ -28,8 +28,12 @@ function Menu() {
               />
             </svg>
           </div>
-          <div className="link-title">Home</div>
-        </a>{" "}
+
+          <div className="link-text">
+            <div className="link-title">Ai Top Ranking</div>
+          </div>
+        </a>
+
         <a href="" className="nav-link">
           <div className="link-icon">
             <svg
@@ -48,8 +52,12 @@ function Menu() {
               />
             </svg>
           </div>
-          <div className="link-title">Dex analizer</div>
+
+          <div className="link-text">
+            <div className="link-title">Dex analizer</div>
+          </div>
         </a>
+
         <a href="" className="nav-link">
           <div className="link-icon">
             <svg
@@ -68,7 +76,134 @@ function Menu() {
               />
             </svg>
           </div>
-          <div className="link-title">Swap</div>
+
+          <div className="link-text">
+            <div className="link-title">Swap</div>
+          </div>
+        </a>
+
+        <a href="" className="nav-link">
+          <div className="link-icon">
+            <svg
+              width="32"
+              height="32"
+              viewBox="0 0 32 32"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M16 21.3333V28M16 21.3333L24 28M16 21.3333L8 28M28 4V14.9333C28 17.1735 28 18.2936 27.564 19.1493C27.1805 19.9019 26.5686 20.5139 25.816 20.8974C24.9603 21.3333 23.8402 21.3333 21.6 21.3333H10.4C8.15979 21.3333 7.03968 21.3333 6.18404 20.8974C5.43139 20.5139 4.81947 19.9019 4.43597 19.1493C4 18.2936 4 17.1735 4 14.9333V4M10.6667 12V16M16 9.33333V16M21.3333 14.6667V16M29.3333 4H2.66667"
+                stroke="#374160"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+            </svg>
+          </div>
+
+          <div className="link-text">
+            <div className="link-title">
+              Social Analizer
+            </div>
+          </div>
+        </a>
+
+        <a href="" className="nav-link">
+          <div className="link-icon">
+            <svg
+              width="32"
+              height="32"
+              viewBox="0 0 32 32"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M16 2.66666C17.751 2.66666 19.4848 3.01153 21.1024 3.6816C22.7201 4.35166 24.19 5.33379 25.4281 6.5719C26.6662 7.81002 27.6483 9.27987 28.3184 10.8976C28.9885 12.5152 29.3333 14.249 29.3333 16M16 2.66666V16M16 2.66666C8.6362 2.66666 2.66667 8.63619 2.66667 16C2.66667 23.3638 8.6362 29.3333 16 29.3333C23.3638 29.3333 29.3333 23.3638 29.3333 16M16 2.66666C23.3638 2.66666 29.3333 8.6362 29.3333 16M29.3333 16L16 16M29.3333 16C29.3333 18.1041 28.8353 20.1784 27.8801 22.0532C26.9248 23.928 25.5394 25.5501 23.8371 26.7869L16 16"
+                stroke="#374160"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+            </svg>
+          </div>
+
+          <div className="link-text">
+            <div className="link-title">
+              Presale Analizer
+            </div>
+          </div>
+        </a>
+
+        <a href="" className="nav-link">
+          <div className="link-icon">
+            <svg
+              width="32"
+              height="32"
+              viewBox="0 0 32 32"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M12 9.33333H6.13333C5.3866 9.33333 5.01323 9.33333 4.72801 9.47866C4.47713 9.60649 4.27316 9.81046 4.14532 10.0613C4 10.3466 4 10.7199 4 11.4667V25.8667C4 26.6134 4 26.9868 4.14532 27.272C4.27316 27.5229 4.47713 27.7268 4.72801 27.8547C5.01323 28 5.3866 28 6.13333 28H12M12 28H20M12 28L12 6.13333C12 5.3866 12 5.01323 12.1453 4.72801C12.2732 4.47713 12.4771 4.27316 12.728 4.14532C13.0132 4 13.3866 4 14.1333 4L17.8667 4C18.6134 4 18.9868 4 19.272 4.14532C19.5229 4.27316 19.7268 4.47713 19.8547 4.72801C20 5.01323 20 5.3866 20 6.13333V28M20 14.6667H25.8667C26.6134 14.6667 26.9868 14.6667 27.272 14.812C27.5229 14.9398 27.7268 15.1438 27.8547 15.3947C28 15.6799 28 16.0533 28 16.8V25.8667C28 26.6134 28 26.9868 27.8547 27.272C27.7268 27.5229 27.5229 27.7268 27.272 27.8547C26.9868 28 26.6134 28 25.8667 28H20"
+                stroke="#374160"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+            </svg>
+          </div>
+
+          <div className="link-text">
+            <div className="link-title">Ai Top Ranking</div>
+          </div>
+        </a>
+
+        <a href="" className="nav-link">
+          <div className="link-icon">
+            <svg
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M21 21L16.65 16.65M19 11C19 15.4183 15.4183 19 11 19C6.58172 19 3 15.4183 3 11C3 6.58172 6.58172 3 11 3C15.4183 3 19 6.58172 19 11Z"
+                stroke="#374160"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+            </svg>
+          </div>
+
+          <div className="link-text">
+            <div className="link-title">Explore</div>
+          </div>
+        </a>
+
+        <a href="" className="nav-link">
+          <div className="link-icon">
+            <svg
+              width="32"
+              height="32"
+              viewBox="0 0 32 32"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M28 12.3333H20M28 5.33331H4M28 19.6666H20M28 26.6666H4M6.13333 21.3333H12.5333C13.2801 21.3333 13.6534 21.3333 13.9387 21.188C14.1895 21.0602 14.3935 20.8562 14.5213 20.6053C14.6667 20.3201 14.6667 19.9467 14.6667 19.2V12.8C14.6667 12.0532 14.6667 11.6799 14.5213 11.3947C14.3935 11.1438 14.1895 10.9398 13.9387 10.812C13.6534 10.6666 13.2801 10.6666 12.5333 10.6666H6.13333C5.3866 10.6666 5.01323 10.6666 4.72801 10.812C4.47713 10.9398 4.27316 11.1438 4.14532 11.3947C4 11.6799 4 12.0532 4 12.8V19.2C4 19.9467 4 20.3201 4.14532 20.6053C4.27316 20.8562 4.47713 21.0602 4.72801 21.188C5.01323 21.3333 5.3866 21.3333 6.13333 21.3333Z"
+                stroke="#374160"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+            </svg>
+          </div>
+
+          <div className="link-text">
+            <div className="link-title">Media</div>
+          </div>
         </a>
       </nav>
     </div>
