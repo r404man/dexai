47d9b2683816c17aceb267319b75517393b4e4bc
Tyrtychko Roman <tyrtychko.r404man@gmail.com>
2023-02-15 18:00:27 +0600

    Fixes links

--- src/components/dashboards/dex/Dex.tsx
+++ src/components/dashboards/dex/Dex.tsx
@@ -991,8 +991,8 @@ export function Overview({ isMobile }: any) {
               </div>
               <div className="wallet-social-icon">
                 <svg
-                  width="24"
-                  height="24"
+                  width="20"
+                  height="20"
                   viewBox="0 0 24 24"
                   fill="none"
                   xmlns="http://www.w3.org/2000/svg"
@@ -1007,8 +1007,8 @@ export function Overview({ isMobile }: any) {
               </div>
               <div className="wallet-social-icon">
                 <svg
-                  width="24"
-                  height="24"
+                  width="20"
+                  height="20"
                   viewBox="0 0 24 24"
                   fill="none"
                   xmlns="http://www.w3.org/2000/svg"
